d07d1645edc257d33bfbf9e13ec73a8ef9f2ed44
ankush1038 <ankusharma1029@gmail.com>
2025-04-22 13:20:41 +0530

    [Ankush] Add: Authentication added

---
+++ src/main/java/com/bridgelabz/employeepayroll/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.bridgelabz.employeepayroll.config;
+
+
+import com.bridgelabz.employeepayroll.Utility.JwtAuthenticationEntryPoint;
+import com.bridgelabz.employeepayroll.Utility.JwtRequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    @Autowired
+    JwtRequestFilter jwtRequestFilter;
+
+    @Autowired
+    JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/register", "/login", "/", "/getAddress", "/addAddresses").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(ex -> ex.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
